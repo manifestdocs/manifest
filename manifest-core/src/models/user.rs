9446486a67f2b7a5947d3d4bf80dee3af2fba69f
Alastair Dawson <283834+kreek@users.noreply.github.com>
2026-01-22 13:03:30 -0800

    Fix clippy warning for from_str method

--- manifest-core/src/models/user.rs
+++ manifest-core/src/models/user.rs
@@ -72,6 +72,7 @@ pub enum MembershipRole {
 }
 
 impl MembershipRole {
+    #[allow(clippy::should_implement_trait)]
     pub fn from_str(s: &str) -> Option<Self> {
         match s {
             "viewer" => Some(Self::Viewer),
